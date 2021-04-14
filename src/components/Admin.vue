bef8faec5646822e6f414ef2337b36d246e3c621
Marcoantonio9 <marcojr918@gmail.com>
2021-04-14 20:50:08 -0300

    Update

---
+++ src/components/Admin.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="admin" class="d-flex capa justify-content-center align-items-center">
+    <form class="container">
+      <div class="row d-flex align-items-center flex-column">
+        <div class="form-group col-4">
+          <label for="exampleInputEmail1" class="ajuste-label">User administrativo:</label>
+          <input type="email" class="form-control" placeholder="Digite o nome de usuário administrativo">
+        </div>
+        <div class="form-group col-4">
+          <label for="exampleInputPassword1" class="ajuste-label">Senha</label>
+          <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Senha">
+        </div>
+        <button type="submit" class="btn btn-danger">Login</button>        
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      
+    }
+  }
+}
+</script>
+
+<style>
+#admin{
+  width: 100%;
+  height: 100vh;
+  background: linear-gradient(rgba(0,0,0,.3),rgba(0,0,0,.3)), url('/imagens/img6.jpg') no-repeat;
+  background-size: 100% 100%;
+}
+.ajuste-label{
+  font-weight: 500;
+  letter-spacing: 1px;
+  color: #fff;
+}
+</style>
--- src/router.js
+++ src/router.js
@@ -1,13 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import PageHome from './components/PageHome'
+import Admin from './components/Admin'
 
 Vue.use(Router)
 
 const router = new Router({
     mode: 'history',
     routes: [
-        {path: '/', component: PageHome, name: 'PageHome'}
+        {path: '/', component: PageHome, name: 'PageHome'},
+        {path: '/admin', component: Admin, name: 'Admin'}
     ]
 
 })
