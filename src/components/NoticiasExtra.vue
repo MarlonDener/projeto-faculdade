6ad9af720f01391b626e9a10d7529263459566af
MarlonDener <=>
2021-06-07 23:52:36 -0300

    Update more section

--- src/components/NoticiasExtra.vue
+++ src/components/NoticiasExtra.vue
@@ -7,25 +7,27 @@
 
 
         <div class="box-container">
-
+         
+          <a href="/noticia/oiQdjMO20wSImtiubwWG">
           <div class="box">
-              <img src="https://images.unsplash.com/photo-1580327344181-c1163234e5a0?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=747&q=80" alt="">
+              <img src="https://images.unsplash.com/photo-1493711662062-fa541adb3fc8?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80" alt="">
               <div class="info">
-                  <h3>01. PS4</h3>
-                  <p><i class="fas fa-quote-left"></i>Lorem ipsum is very nice, because so much things, 
-                  stay more easy in write texts in your website. I hope one day, meet m ipsum is very nice,, m ipsum is very nice,<i class="fas fa-quote-right"></i></p>
+                  <h3>01. Vídeo Games</h3>
+                  <p><i class="fas fa-quote-left"></i>Vídeo games se tornaram ferramentas para lidar com o isolamento social durante a pandemia<i class="fas fa-quote-right"></i></p>
                   
               </div>
           </div>
           
+          </a>
+          <a href="/noticia/2MSfpGyH1go4bJlE7obm">
           <div class="box">
-              <img src="https://images.unsplash.com/photo-1580327344181-c1163234e5a0?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=747&q=80" alt="">
+              <img src="https://img.ibxk.com.br/2021/06/07/07161134714352.jpg?w=704&h=264&mode=crop&scale=both" alt="">
               <div class="info">
-                  <h3>02. Nitendo</h3>
-                  <p><i class="fas fa-quote-left"></i>Lorem ipsum is very nice, because so much things, 
-                  stay more easy in write texts in your website. I hope one day, meet m ipsum is very nice,, m ipsum is very nice,<i class="fas fa-quote-right"></i></p>
+                  <h3>02. Battlefield</h3>
+                  <p><i class="fas fa-quote-left"></i>De acordo com um rumor, Battlefield 6 pode se chamar Battlefield 2042. O boato veio do Discord do insider Tom Henderson, o responsável por tornar público...<i class="fas fa-quote-right"></i></p>
               </div>
           </div>
+          </a>
           
 
         </div>
@@ -33,25 +35,26 @@
         <img src="/imagens/divider.png" alt="divider" class="divider"/>
 
         <div class="box-container segundo">
+            <a href="/noticia/W1msjIR4YzB9y1aNT3aj">
+            <div class="box">
+                <img src="https://www.gamerpoint.com.br/wp-content/uploads/2021/05/Call-of-duty-2021-Sledgehammer-Games-.jpg" alt="">
+                <div class="info">
+                    <h3>03. Call of Duty</h3>
+                    <p><i class="fas fa-quote-left"></i>Call of Duty: Warzone já tem mais de 500 mil jogadores banidos<i class="fas fa-quote-right"></i></p>
+                </div>
+          </div>
+          </a>
 
+         <a href="/noticia/a830mgU0Mr5tLuOIvpFh">
           <div class="box">
-              <img src="https://images.unsplash.com/photo-1580327344181-c1163234e5a0?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=747&q=80" alt="">
+              <img src="https://cdn.ome.lt/LLNZvNVy0JVkLATyKvy7uXrUVO0=/970x360/smart/uploads/conteudo/fotos/valorant_HAr0mZg.jpg" alt="">
               <div class="info">
-                  <h3>03. Venda de jogos</h3>
-                  <p><i class="fas fa-quote-left"></i>Lore because so much things, 
-                  stay more easy in write texts in your website. I hope one day, meet my wm ipsum is very nice,ay
-                   the life, m ipsum is very nice,<i class="fas fa-quote-right"></i></p>
+                  <h3>04. Valorant</h3>
+                  <p><i class="fas fa-quote-left"></i>Valorant: Riot tem planejamento para expandir história do jogo<i class="fas fa-quote-right"></i></p>
               </div>
           </div>
-
-          <div class="box">
-              <img src="https://images.unsplash.com/photo-1580327344181-c1163234e5a0?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=747&q=80" alt="">
-              <div class="info">
-                  <h3>04. Nova geração</h3>
-                  <p><i class="fas fa-quote-left"></i>Lorem ipsum is very nice, because so much things, 
-                  stay more easy in write texts in your website. I hope one day, meet m ipsum is very nice,, m ipsum is very nice,<i class="fas fa-quote-right"></i></p>
-              </div>
-          </div>
+          
+          </a>
           
         </div>
 
